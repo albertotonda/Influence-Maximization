df5eac45e352af6faa1f3b6720293560a822de76
alberto <alberto.tonda@gmail.com>
2019-09-24 16:12:17 +0200

    Single-objective EA-based influence maximization is now operational and running. It should be tested properly.

--- evolutionaryalgorithm.py
+++ evolutionaryalgorithm.py
@@ -61,6 +61,7 @@ def moea_influence_maximization(G, p, no_simulations, model, population_size=100
         maximize = True,
         seeds = initial_population,
         pop_size = population_size,
+        num_selected = offspring_size,
         max_generations = max_generations,
 
         # all arguments below will go inside the dictionary 'args'
@@ -148,7 +149,8 @@ def ea_observer(population, num_generations, num_evaluations, args) :
     best = max(population)
     logging.info('[{0:.2f} s] Generation {1:6} -- {2}'.format(timeElapsed, num_generations, best.fitness))
 
-    # TODO write current state of the population to a file
+    # TODO write current state of the ALGORITHM to a file (e.g. random number generator, time elapsed, stuff like that)
+    # write current state of the population to a file
     population_file = args["population_file"]
     
     # find the longest individual
@@ -162,7 +164,13 @@ def ea_observer(population, num_generations, num_evaluations, args) :
 
         # and now, we write stuff, individual by individual
         for individual in  population :
-            fp.write("%d,%.4f" % (1.0 / individual.fitness[1], individual.fitness[0]))
+
+            # check if fitness is an iterable collection (e.g. a list) or just a single value
+            if hasattr(individual.fitness, "__iter__") :
+                fp.write("%d,%.4f" % (1.0 / individual.fitness[1], individual.fitness[0]))
+            else :
+                fp.write("%d,%.4f" % ( len(set(individual.candidate)), individual.fitness))
+
             for node in individual.candidate :
                 fp.write(",%d" % node)
 
@@ -191,12 +199,12 @@ def nsga2_super_operator(random, candidate1, candidate2, args) :
     elif randomChoice == 3 :
         children.append( nsga2_removal_mutation(random, list(candidate1), args) )
     
-    # purge the children from "None" and empty arrays
-    children = [c for c in children if c is not None and len(c) > 0]
-    
     # this should probably be commented or sent to logging
     for c in children : logging.debug("randomChoice=%d : from parent of size %d, created child of size %d" % (randomChoice, len(candidate1), len(c)) )
 
+    # purge the children from "None" and empty arrays
+    children = [c for c in children if c is not None and len(c) > 0]
+    
     return children
 
 #@inspyred.ec.variators.crossover # decorator that defines the operator as a crossover
@@ -290,13 +298,175 @@ def nsga2_generator(random, args) :
 
     return individual
 
+"""Single-objective evolutionary influence maximization. Parameters:
+    k: seed set size
+    G: networkx graph
+    p: probability of influence spread 
+    no_simulations: number of simulations
+    model: type of influence propagation model
+    population_size: population of the EA (default: value)
+    offspring_size: offspring of the EA (default: value)
+    max_generations: maximum generations (default: value)
+    n_threads: number of threads to be used for concurrent evaluations (default: 1)
+    random_seed: seed to initialize the pseudo-random number generation (default: time)
+    initial_population: individuals (seed sets) to be added to the initial population (the rest will be randomly generated)
+    """
+def ea_influence_maximization(k, G, p, no_simulations, model, population_size=100, offspring_size=100, max_generations=100, n_threads=1, random_seed=None, initial_population=None, population_file=None) :
+
+    # initialize a generic evolutionary algorithm
+    logging.debug("Initializing Evolutionary Algorithm...")
+    prng = random.Random()
+    if random_seed == None : random_seed = time()
+    logging.debug("Random number generator seeded with %s" % str(random_seed))
+    prng.seed(random_seed)
+
+    # check if some of the optional parameters are set; otherwise, use default values
+    nodes = list(G.nodes)
+    if population_file == None :
+        ct = time()
+        population_file = strftime("%Y-%m-%d-%H-%M-%S-population.csv")
+
+    # instantiate a basic EvolutionaryComputation object, that is "empty" (no default methods defined for any component)
+    # so we will need to define every method
+    ea = inspyred.ec.EvolutionaryComputation(prng)
+    ea.observer = ea_observer
+    ea.variator = [ea_super_operator]
+    ea.terminator = inspyred.ec.terminators.generation_termination
+    ea.selector = inspyred.ec.selectors.tournament_selection # default size is 2
+    ea.replacer = inspyred.ec.replacers.plus_replacement
+
+    # start the evolutionary process
+    final_population = ea.evolve(
+        generator = ea_generator,
+        evaluator = ea_evaluator,
+        maximize = True,
+        seeds = initial_population,
+        pop_size = population_size,
+        num_selected = offspring_size,
+        max_generations = max_generations,
+
+        # all arguments below will go inside the dictionary 'args'
+        k = k,
+        G = G,
+        p = p,
+        model = model,
+        no_simulations = no_simulations,
+        nodes = nodes,
+        n_threads = n_threads,
+        population_file = population_file,
+        time_previous_generation = time(), # this will be updated in the observer
+    )
+
+    best_individual = max(final_population)
+    best_seed_set = best_individual.candidate
+    spread = best_individual.fitness
+
+    return best_seed_set, spread
+
+@inspyred.ec.generators.diversify # decorator that makes it impossible to generate copies
+def ea_generator(random, args) :
+
+    # k is the size of the seed sets
+    k = args["k"]
+    nodes = args["nodes"]
+
+    # extract random number in 1,max_seed_nodes
+    individual = [0] * k
+    logging.debug( "Creating individual of size %d, with genes ranging from %d to %d" % (k, nodes[0], nodes[-1]) )
+    for i in range(0, k) : individual[i] = nodes[ random.randint(0, len(nodes)-1) ]
+    logging.debug(individual)
+
+    return individual
+
+@inspyred.ec.variators.crossover # decorator that defines the operator as a crossover, even if it isn't in this case :-)
+def ea_super_operator(random, candidate1, candidate2, args) :
+
+    k = args["k"]
+    children = []
+
+    # uniform choice of operator
+    randomChoice = random.randint(0,1)
+    
+    # one-point crossover or mutation that swaps exactly one node with another
+    if randomChoice == 0 :
+        children = inspyred.ec.variators.n_point_crossover(random, [list(candidate1), list(candidate2)], args)
+    elif randomChoice == 1 :
+        children.append( ea_alteration_mutation(random, list(candidate1), args) )
+    
+    # this should probably be commented or sent to logging
+    for c in children : logging.debug("randomChoice=%d : from parent of size %d, created child of size %d" % (randomChoice, len(candidate1), len(c)) )
+
+    # purge the children from "None" and arrays of the wrong size
+    children = [c for c in children if c is not None and len(set(c)) == k]
+
+    return children
+
+def ea_evaluator(candidates, args) :
+
+    n_threads = args["n_threads"]
+    G = args["G"]
+    p = args["p"]
+    model = args["model"]
+    no_simulations = args["no_simulations"]
+
+    # we start with a list where every element is None
+    fitness = [None] * len(candidates)
+
+    # depending on how many threads we have at our disposal,
+    # we use a different methodology
+    # if we just have one thread, let's just evaluate individuals old style 
+    if n_threads == 1 :
+        for index, A in enumerate(candidates) :
+
+            # TODO sort phenotype, use cache...? or manage sorting directly during individual creation? see lines 108-142 in src_OLD/multiObjective-inspyred/sn-inflmax-inspyred.py 
+            # TODO maybe if we make sure that candidates are already sets before getting here, we could save some computational time
+            A_set = set(A)
+
+            # TODO consider std inside the fitness in some way?
+            influence_mean, influence_std = spread.MonteCarlo_simulation(G, A_set, p, no_simulations, model)
+            fitness[index] = influence_mean
+
+    else :
+        
+        # create a threadpool, using the local module
+        import threadpool
+        thread_pool = threadpool.ThreadPool(n_threads)
+
+        # create thread lock, to be used for concurrency
+        import threading
+        thread_lock = threading.Lock()
+
+        # create list of tasks for the thread pool, using the threaded evaluation function
+        tasks = [ (G, p, A, no_simulations, model, fitness, index, thread_lock) for index, A in enumerate(candidates) ]
+        thread_pool.map(ea_evaluator_threaded, tasks)
+
+        # start thread pool and wait for conclusion
+        thread_pool.wait_completion()
+
+    return fitness
+
+def ea_evaluator_threaded(G, p, A, no_simulations, model, fitness, index, thread_lock, thread_id) :
+
+    # TODO not sure that this is needed
+    A_set = set(A)
+
+    # run spread simulation
+    influence_mean, influence_std = spread.MonteCarlo_simulation(G, A_set, p, no_simulations, model)
+
+    # lock shared resource, write in it, release
+    thread_lock.acquire()
+    fitness[index] = influence_mean
+    thread_lock.release()
+
+    return
+
 # this main here is just to test the current implementation
 if __name__ == "__main__" :
 
     # initialize logging
     import logging
     logger = logging.getLogger('')
-    logger.setLevel(logging.INFO) # TODO change to .DEBUG for more in-depth logging
+    logger.setLevel(logging.DEBUG) # TODO switch between INFO and DEBUG for less or more in-depth logging
     formatter = logging.Formatter('[%(levelname)s %(asctime)s] %(message)s', '%Y-%m-%d %H:%M:%S') 
  
     ch = logging.StreamHandler()
@@ -305,12 +475,14 @@ if __name__ == "__main__" :
     logger.addHandler(ch)
 
     import load
+    k = 5
     G = load.read_graph("graphs/facebook_combined.txt")
     p = 0.01
     model = 'WC'
     no_simulations = 100
     max_generations = 5
-    n_threads = 4
+    n_threads = 1
     random_seed = 42
 
-    seed_sets = moea_influence_maximization(G, p, no_simulations, model, population_size=16, random_seed=random_seed, max_generations=max_generations, n_threads=n_threads)
+    #seed_sets = moea_influence_maximization(G, p, no_simulations, model, population_size=16, offspring_size=16, random_seed=random_seed, max_generations=max_generations, n_threads=n_threads)
+    seed_set, spread = ea_influence_maximization(k, G, p, no_simulations, model, population_size=16, offspring_size=16, random_seed=random_seed, max_generations=max_generations, n_threads=n_threads)
