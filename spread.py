66d0842ce9898413ad6af42a193e828c5b26406b
albertotonda <alberto.tonda@gmail.com>
2020-05-12 10:58:40 +0200

    Solved minor issue with random number generator in spread.py

--- spread.py
+++ spread.py
@@ -9,7 +9,7 @@ import math
 	Suits (un)directed graphs. 
 	Assumes the edges point OUT of the influencer, e.g., if A->B or A-B, then "A influences B".
 """
-def IC_model(G, a, p):              # a: the set of initial active nodes
+def IC_model(G, a, p, random_generator):              # a: the set of initial active nodes
 	                                # p: the system-wide probability of influence on an edge, in [0,1]
 	A = set(a)                      # A: the set of active nodes, initially a
 	B = set(a)                      # B: the set of nodes activated in the last completed iteration
@@ -19,7 +19,7 @@ def IC_model(G, a, p):              # a: the set of initial active nodes
 		nextB = set()
 		for n in B:
 			for m in set(G.neighbors(n)) - A: # G.neighbors follows A-B and A->B (successor) edges
-				prob = random.random() # in the range [0.0, 1.0)
+				prob = random_generator.random() # in the range [0.0, 1.0)
 				if prob <= p:
 					nextB.add(m)
 		B = set(nextB)
@@ -29,7 +29,7 @@ def IC_model(G, a, p):              # a: the set of initial active nodes
 
 	return len(A)
 
-def WC_model(G, a):                 # a: the set of initial active nodes
+def WC_model(G, a, random_generator):                 # a: the set of initial active nodes
                                     # each edge from node u to v is assigned probability 1/in-degree(v) of activating v
 	A = set(a)                      # A: the set of active nodes, initially a
 	B = set(a)                      # B: the set of nodes activated in the last completed iteration
@@ -44,7 +44,7 @@ def WC_model(G, a):                 # a: the set of initial active nodes
 		nextB = set()
 		for n in B:
 			for m in set(G.neighbors(n)) - A:
-				prob = random.random() # in the range [0.0, 1.0)
+				prob = random_generator.random() # in the range [0.0, 1.0)
 				p = 1.0/my_degree_function(m)
 				if prob <= p:
 					nextB.add(m)
@@ -107,15 +107,20 @@ def WC_model_max_hop(G, a, max_hop, random_generator):  # a: the set of initial 
 """ Evaluates a given seed set A, simulated "no_simulations" times.
 	Returns a tuple: (the mean, the stdev).
 """
-def MonteCarlo_simulation(G, A, p, no_simulations, model):
+def MonteCarlo_simulation(G, A, p, no_simulations, model, random_generator=None):
+
+	if random_generator is None:
+		random_generator = random.Random()
+		random_generator.seed(next(iter(A))) # initialize random number generator with first seed in the seed set, to make experiment repeatable; TODO evaluate computational cost
+
 	results = []
 
 	if model == 'WC':
 		for i in range(no_simulations):
-			results.append(WC_model(G, A))
+			results.append(WC_model(G, A, random_generator=random_generator))
 	elif model == 'IC':
 		for i in range(no_simulations):
-			results.append(IC_model(G, A, p))
+			results.append(IC_model(G, A, p, random_generator=random_generator))
 
 	return (numpy.mean(results), numpy.std(results))
 
